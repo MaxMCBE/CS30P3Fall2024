921a973cb57622f67dc8fe6ab9f57463377041cb
27127006 <27127006@L815WD220015.edu.cbe.ab.ca>
2024-09-17 15:30:59 -0600

    15:30

--- Chapter10/src/LocalBank/LocalBank.java
+++ Chapter10/src/LocalBank/LocalBank.java
@@ -19,24 +19,28 @@ Course: Computer Science 30
 
 public class LocalBank 
 {
-	private Bank bank = new Bank();
-	private Account selectedAccount = null;
+	private Bank bank = new Bank(); //Create a bank object
+	private Account selectedAccount = null; //Set selected account to null at the beginning
 	
+	//Define the many GUI elements so that they can be used anywhere in the init function without worrying about what is above what (although I don't believe it should conflict much anyways)
 	private JFrame frame;
 	
 	private JSplitPane splitPane;
 	
+	//Left bar
 	private JPanel leftBar;
 	private JLabel title;
 	private JButton addAccount;
 	private JButton viewAccount;
 	
 	private JPanel accountWindow;
+	//Right bar labels
 	private JLabel idLabel;
 	private JLabel nameLabel;
 	private JLabel addressLabel;
 	private JLabel balanceLabel;
 	
+	//Right bar buttons
 	private JButton depositButton;
 	private JButton withdrawButton;
 	private JButton closeButton;
@@ -44,6 +48,7 @@ public class LocalBank
 	private JButton pinButton;
 	private JButton deleteButton;
 	
+	//Acccount creation input panel for use with JOptionPane (Breaks windowbuilder, addAccount must be commented out to view the main window in Design view)
 	private JPanel inputPanel;
 	private JTextField idInput;
 	private JTextField pinInput;
@@ -55,18 +60,20 @@ public class LocalBank
 	private JTextField zipInput;
 	private JTextField balanceInput;
 
+	//Main
 	public static void main(String[] args) 
 	{
-		EventQueue.invokeLater(new Runnable() 
+		EventQueue.invokeLater(new Runnable() //Create thread
 		{
-			public void run() 
+			public void run() //Run method
 			{
-				try 
+				//The try/catch is default windowbuilder code. I don't 100% understand the purpose, so I apologize if the comments are insufficient
+				try //Try to create window
 				{
-					LocalBank window = new LocalBank();
-					window.frame.setVisible(true);
+					LocalBank window = new LocalBank(); //Create a LocalBank GUI
+					window.frame.setVisible(true); //Set it to be visible (done here instead of constructor/init to stop the unused error on window
 				} 
-				catch (Exception e) 
+				catch (Exception e) //If interrupted, print error
 				{
 					e.printStackTrace();
 				}
@@ -74,34 +81,40 @@ public class LocalBank
 		});
 	}
 
-	public LocalBank() 
+	public LocalBank() //Constructor
 	{
-		initialize();
+		initialize(); //Run initialize method
 	}
 
+	//The big method that builds all the GUI (sorry code standards, this is going to be a lot longer than suggested. I assume it's expected with windowbuilder though)
 	private void initialize() 
 	{
+		//Create main frame
 		frame = new JFrame("LocalBank");
-		frame.setBounds(0, 0, 600, 450);
-		frame.setLocationRelativeTo(null);
-		((JComponent) frame.getContentPane()).setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+		frame.setBounds(0, 0, 600, 450); //Dimensions (can be modified easily by dragging the window larger or smaller, but this tends to work from my testing)
+		frame.setLocationRelativeTo(null); //Center on screen
+		((JComponent) frame.getContentPane()).setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15)); //Create border
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		splitPane = new JSplitPane();
+		splitPane = new JSplitPane(); //Create a SplitPane to fill the frame
 		frame.getContentPane().add(splitPane, BorderLayout.CENTER);
 		
+		//Left side of the SplitPane
 		leftBar = new JPanel();
 		splitPane.setLeftComponent(leftBar);
-		leftBar.setLayout(new GridLayout(3, 1, 15, 15)); //2x2 grid layout
+		leftBar.setLayout(new GridLayout(0, 1, 15, 15)); //Grid layout, which functions as a Y box layout with padding here
 		
+		//Title
 		title = new JLabel("LocalBank");
 		title.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		title.setHorizontalAlignment(SwingConstants.CENTER);
 		leftBar.add(title);
 		
+		//Input panel for account creation (breaks windowbuilder)
 		inputPanel = new JPanel();
-		inputPanel.setLayout(new GridLayout(0, 1, 5, 5));
+		inputPanel.setLayout(new GridLayout(0, 1, 5, 5)); //Grid layout
 		
+		//The many input text fields
 		idInput = new JTextField();
 		pinInput = new JTextField();
 		fnInput = new JTextField();
@@ -112,6 +125,7 @@ public class LocalBank
 		zipInput = new JTextField();
 		balanceInput = new JTextField();
 		
+		//Add all textfields and corresponding labels to the input panel
 		inputPanel.add(new JLabel("New account ID:"));
 		inputPanel.add(idInput);
 		inputPanel.add(new JLabel("Account PIN:"));
@@ -134,31 +148,35 @@ public class LocalBank
 		inputPanel.add(new JLabel("Starting account balance:"));
 		inputPanel.add(balanceInput);
 		
+		//Button to create an account (uses inputPanel)
 		addAccount = new JButton("Add Account");
 		addAccount.addActionListener(new ActionListener()
 		{
-			public void actionPerformed(ActionEvent e) 
+			public void actionPerformed(ActionEvent e) //On click
 			{
-				int input = JOptionPane.showConfirmDialog(null, inputPanel, "Account Information", JOptionPane.OK_CANCEL_OPTION);
-				if (input == JOptionPane.OK_OPTION) 
+				int input = JOptionPane.showConfirmDialog(null, inputPanel, "Account Information", JOptionPane.OK_CANCEL_OPTION); //Show a JOptionPane with the inputPanel as content and yes/no option
+				if (input == JOptionPane.OK_OPTION) //If input is submitted
 				{
-					if (idInput.getText().equals("") || fnInput.getText().equals("") || lnInput.getText().equals(""))
+					//This is gross but I'm not sure how to do it better. Just checks if any field is empty EXCEPT the numbers as they'll be checked later when converting to ints
+					if (idInput.getText().equals("") || fnInput.getText().equals("") || lnInput.getText().equals("") || streetInput.getText().equals("") || cityInput.getText().equals("") || provinceInput.getText().equals("") || zipInput.getText().equals(""))
 					{
 						JOptionPane.showMessageDialog(null, "Please fill all fields", "Input Error", JOptionPane.ERROR_MESSAGE); //Error message
 					}
-					else
+					else //Assuming everything is nice
 					{
-						try
+						try //Try to do the following (if any parseInts fail or an exception is thrown, catch it)
 						{
-							if (bank.accounts.get(idInput.getText()) == null)
+							if (bank.accounts.get(idInput.getText()) == null) //If there is no account with the requested ID
 							{
-								if (pinInput.getText().length() == 4 && Integer.parseInt(pinInput.getText()) >= 0)
+								if (pinInput.getText().length() == 4 && Integer.parseInt(pinInput.getText()) >= 0) //If the PIN is 4 digits long (and doesn't use a negative to hit 4 characters!)
 								{
 									bank.addAccount(
 											idInput.getText(), Integer.parseInt(pinInput.getText()), 
 											fnInput.getText(), lnInput.getText(),
 											streetInput.getText(), cityInput.getText(), provinceInput.getText(), zipInput.getText(),
-											Double.parseDouble(balanceInput.getText()));
+											Double.parseDouble(balanceInput.getText())
+											); //Add an account. Used new lines to make the parameters a bit more manageable
+									//Reset all the fields, so that if you try to create another account it won't still have all the old data. Doesn't really matter here but it'd be a huge security issue in a real program obviously
 									idInput.setText(null);
 									pinInput.setText(null);
 									fnInput.setText(null);
@@ -169,24 +187,25 @@ public class LocalBank
 									zipInput.setText(null);
 									balanceInput.setText(null);
 								}
-								else
+								else //If the PIN is not 4 digits
 								{
-									throw new Exception();
+									throw new Exception(); //Just throw an exception, easier and less repetition than just making a slightly different error message appear
 								}
 							}
-							else
+							else //If there is an account with the requested ID
 							{
 								JOptionPane.showMessageDialog(null, "Account with this ID already exists", "Duplicate Error", JOptionPane.ERROR_MESSAGE); //Error message
-								idInput.setText(null);
+								idInput.setText(null); //Reset that field (everything else will be there when reopened so it's less annoying to fix)
 							}
 						}
-						catch (Exception invalidBalance)
+						catch (Exception invalidInput) //If any of the numbers are not numbers, or if the PIN is not 4 digits
 						{
 							JOptionPane.showMessageDialog(null, "Please enter a number for current balance and a 4 digit number for PIN", "Input Error", JOptionPane.ERROR_MESSAGE); //Error message
 						}
 					}
 				}
-				update();
+				//No else- if someone clicks to cancel out of the input window, nothign happens
+				update(); //Update GUI
 			}
 		});
 		addAccount.setToolTipText("Create a new account");
@@ -324,6 +343,7 @@ public class LocalBank
 		accountWindow.add(withdrawButton);
 		
 		closeButton = new JButton("Unload account");
+		closeButton.setToolTipText("Unload the account and return to home");
 		closeButton.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
@@ -338,6 +358,7 @@ public class LocalBank
 		accountWindow.add(closeButton);
 		
 		addressButton = new JButton("Change address");
+		addressButton.setToolTipText("Change account address");
 		addressButton.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
@@ -356,6 +377,7 @@ public class LocalBank
 		accountWindow.add(addressButton);
 		
 		pinButton = new JButton("Change PIN");
+		pinButton.setToolTipText("Change account PIN");
 		pinButton.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
