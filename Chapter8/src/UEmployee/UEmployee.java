859b7ceb2805e0a01eacb62afa558511485b0bb5
27127006 <27127006@L815WD220015.edu.cbe.ab.ca>
2024-11-04 13:59:47 -0700

    13:59

--- Chapter8/src/UEmployee/UEmployee.java
+++ Chapter8/src/UEmployee/UEmployee.java
@@ -1,33 +1,66 @@
 package UEmployee;
 
-public abstract class UEmployee 
+/*
+
+Program: UEmployee.java          Last Date of this Revision: November 4, 2024
+
+Purpose: Abstract class for a university employee, inherited by Faculty and Staff
+
+Author: Max MacPhee 
+School: CHHS
+Course: Computer Science 30
+
+*/
+
+public abstract class UEmployee //Abstract because an employee will always be a faculty or staff and not just an employee
 {
-	private String name;
-	private int salary;
+	private String name; //Name of employee
+	private int salary; //Salary of employee
 	
+	/*
+	 * Constructor
+	 * @param n Name of employee
+	 * @param s Salary of employee
+	 */
 	public UEmployee(String n, int s)
 	{
-		name = n;
+		name = n; //Name and salary from parameters
 		salary = s;
 	}
 	
+	/*
+	 * Returns the employee's name
+	 * @return name of employee as a string
+	 */
 	public String getName()
 	{
-		return name;
+		return name; //Return name
 	}
 	
+	/*
+	 * Sets the employee's name to a new value
+	 * @param n New name value of employee
+	 */
 	public void setName(String n)
 	{
-		name = n;
+		name = n; //Set name to parameter
 	}
 	
+	/*
+	 * Returns the employee's salary
+	 * @return employee's salary as an integer
+	 */
 	public int getSalary()
 	{
-		return salary;
+		return salary; //Return salary
 	}
 	
+	/*
+	 * Sets the employee's salary to a new value
+	 * @param s New salary of employee
+	 */
 	public void setSalary(int s)
 	{
-		salary = s;
+		salary = s; //Set salary to parameter
 	}
 }
