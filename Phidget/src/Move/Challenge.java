d5d5f2863f057ab35b3bde91d6b518ebb9a0f29a
27127006 <27127006@L815WD220015.edu.cbe.ab.ca>
2024-11-20 12:42:47 -0700

    12:42

--- Phidget/src/Move/Challenge.java
+++ Phidget/src/Move/Challenge.java
@@ -39,36 +39,44 @@ public class Challenge
 		rightMotors.open(1000);
 		sonar.open(1000);
 		
+		//Immediately set acceleration to maximum
 		leftMotors.setAcceleration(leftMotors.getMaxAcceleration());
         rightMotors.setAcceleration(rightMotors.getMaxAcceleration());
 		
-		int i = 0;
-		int turnDirection = 1;
+		int i = 0; //Variable to tell how long the rover has been driving
+		final int GOAL = 10; //Goal for how long it should drive (10 cycles, or 1000ms/1s)
+		int turnDirection = 1; //Direction to turn (1 or -1, which is used to multiply the velocity of each motor)
 		
-		while (true) 
+		while (true) //Infinite loop
 		{
-            if (sonar.getDistance() < 250)
+            if (sonar.getDistance() < 150) //If the rover detects an obstacle
             {
-                leftMotors.setTargetVelocity(0.5); //Turn around
+            	//Turn around
+                leftMotors.setTargetVelocity(0.5);
                 rightMotors.setTargetVelocity(-0.5);
                 Thread.sleep(1200);
+                //Reverse turn direction (so it maintains the same path)
                 turnDirection *= -1;
+                //Set the new i to however much was left - reversing the start and end
+                i = GOAL - i;
             }
-            else
+            else //If it does not detect an object
             {
             	leftMotors.setTargetVelocity(-0.25); //Continue driving forwards
                 rightMotors.setTargetVelocity(-0.25);
             }
             
-            i++;
-            Thread.sleep(100); //Wait 100ms then repeat
+            i++; //Increase i
+            Thread.sleep(100); //Wait 100ms
             
-            if (i == 30)
+            if (i == GOAL) //If it has driven long enough to reach the goal
             {
-            	i = 0;
-            	leftMotors.setTargetVelocity(0.5*turnDirection); //Turn around
+            	i = 0; //Reset i
+            	//Turn 90 degrees (direction depends on turnDirection variable)
+            	leftMotors.setTargetVelocity(0.5*turnDirection);
                 rightMotors.setTargetVelocity(-0.5*turnDirection);
-                Thread.sleep(600);
+                Thread.sleep(625);
+                //Restart the loop
             }
         }
 	}
